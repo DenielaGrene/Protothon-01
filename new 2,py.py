efd5a852d985a07fb44fa95ec6d65a1006796ea5
DenielaGrene <47411910+DenielaGrene@users.noreply.github.com>
2019-03-16 12:05:29 +0530

    Add files via upload

---
+++ new 2,py.py
@@ -0,0 +1,24 @@
+import math  
+def rmsValue(arr, n): 
+    square = 0
+    mean = 0.0
+    root = 0.0
+      
+     
+    for i in range(0,n): 
+        square += (arr[i]**2) 
+      
+   
+    mean = (square / (float)(n)) 
+      
+
+    root = math.sqrt(mean) 
+      
+    return root 
+  
+
+if __name__=='__main__': 
+    arr = [56,76,45,32]
+
+    n = len(arr) 
+    print(rmsValue(arr, n)) 
